a03e6ee64e400ef37ed8c5d7ca6e6cdba3b23094
Phoebin <ghilavo@gmail.com>
2026-05-12 23:51:33 +0700

    My Linked List

--- java/src/Project4/MyLinkList/MyLinkedList.java
+++ java/src/Project4/MyLinkList/MyLinkedList.java
@@ -4,36 +4,83 @@ import java.util.NoSuchElementException;
 
 public class MyLinkedList implements MyList {
 
-    private Node head = null;
+    private Node head ;
 
-    private int size = 0;
+    private int size ;
+
+
+    public MyLinkedList() {
+        head = null; // Initially, the linked list is empty, so the head points to null.
+        size = 0; // The size of the linked list is zero.
+    }
 
     @Override
     public void addToEnd(Object o) {
-        Node node = new Node();
-        node.data = o;
+        Node newNode = new Node();
+        newNode.data = o;
         if(head == null) {
-            head = node;
+            head = newNode;
         }else{
-            head.next = node;
+            Node current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
         }
-
+        size++;
     }
 
     @Override
     public void insertAt(int index, Object o) {
         if(index < 0 || index > size) throw new NoSuchElementException();
 
+        Node newNode = new Node();
+        newNode.data = o;
+
+        if(index == 0){
+            newNode.next = head;
+            head = newNode;
+        }else{
+            Node current = head;
+            for(int i = 0 ; i<index -1 ; i++){
+                current = current.next;
+            }
+
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        size++;
     }
 
     @Override
     public void removeAt(int index) {
+        if(index < 0 || index >= size) throw new NoSuchElementException();
+
+        if(index == 0){
+            head = head.next;
+        }else{
+            Node current = head;
+            for (int i = 0 ; i<index -1 ;i++){
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+        size--;
 
     }
 
     @Override
     public Object getAt(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new NoSuchElementException("Index out of bounds");
+        }
+
+        Node current = head;
+        for (int i = 0 ; i<index -1 ; i++){
+            current= current.next;
+        }
+
+        return current.data;
     }
 
     @Override
@@ -43,6 +90,31 @@ public class MyLinkedList implements MyList {
 
     @Override
     public MyListIterator getIterator() {
-        return null;
+        return new MyLinkedListIterator();
     }
+
+    private class MyLinkedListIterator implements MyListIterator {
+        Node currentNode = null;
+
+        // Get the next element in the linked list.
+        @Override
+        public Object next() {
+            if (currentNode != null)
+                currentNode = currentNode.next;
+            else
+                currentNode = head;
+
+            return currentNode.data;
+        }
+
+        // Check if there are more elements in the linked list to iterate.
+        @Override
+        public boolean hasNext() {
+            if (currentNode != null)
+                return currentNode.next != null;
+            else
+                return head != null;
+        }
+    }
+
 }
